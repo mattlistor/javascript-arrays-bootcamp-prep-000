5d04a57c6527490df143f43e1379b41692e687b2
Matthew Listor <mattlistor@gmail.com>
2019-04-16 02:01:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+
+
 function addElementToBeginningOfArray(a, b){
-  
+  a.unshift(b)
+  return a
 }
 
 function destructivelyAddElementToBeginningOfArray(a, b){
