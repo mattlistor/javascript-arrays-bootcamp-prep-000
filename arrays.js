3f11ea8c64ff700a66f774835985fac4bf0935e9
Matthew Listor <mattlistor@gmail.com>
2019-04-16 02:08:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, newItem){
+function addElementToBeginningOfArray(choloateBars, newItem){
   return [b, ...chocolateBars];
 }
 
